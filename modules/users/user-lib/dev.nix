79c49d4fd6d61ef756c0eb2b7b01e987e0d541e0
atta-ub <atta_ub@hotmail.com>
2026-06-08 04:10:09 -0700

    added packages and foramtters  related to kaskell, rust and python

--- modules/users/user-lib/dev.nix
+++ modules/users/user-lib/dev.nix
@@ -6,6 +6,8 @@
     julia
     ghc
     haskellPackages.cabal-install
+    haskell-language-server  # HLS LSP server (used by lsp-haskell)
+    ormolu                   # Haskell formatter
     clisp
     cmake
     gcc
@@ -13,6 +15,9 @@
     libtool
     glib
     clang-tools  # provides clangd (LSP server) and clang-format
+    lldb         # debug adapter for C/C++/Rust (used by dap-lldb)
+    pyright      # Python LSP server (used by eglot)
+    ruff         # Python formatter (used by ruff-format Emacs package)
 
 
     graphviz
@@ -27,8 +32,10 @@
     black
     claude-code
     xorgserver  # for Xvf
-    rustc 
+    rustc
     cargo
+    rust-analyzer  # Rust LSP server (used by lsp-mode)
+    rustfmt        # Rust formatter (used by rust-mode)
 
   ];
 }
